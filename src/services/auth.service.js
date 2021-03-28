0daa318aeda562b15d334cfac95aca80e8873b06
José Ramón Fernández <josferde5@alum.us.es>
2021-03-29 00:06:18 +0200

    #20 - Configuración de despliegue en Heroku

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,11 +1,11 @@
-import axios from "axios";
+import http from "../http-common";
 
-const API_URL = "http://localhost:8080/api/auth/";
+// const API_URL = "http://localhost:8080/api/auth/";
 
 class AuthService {
     login(username, password) {
-        return axios
-            .post(API_URL + "signin", {
+        return http
+            .post("/signin", {
                 username,
                 password
             })
@@ -23,7 +23,7 @@ class AuthService {
     }
 
     register(username, email, password) {
-        return axios.post(API_URL + "signup", {
+        return http.post("/signup", {
             username,
             email,
             password

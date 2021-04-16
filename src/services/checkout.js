774f6eadeebfd57a8861eeda24eff453c7718064
josgamdia1 <josgamdia1@alum.us.es>
2021-04-16 16:40:57 +0200

    Cambiado delete servicio checkout

--- src/services/checkout.js
+++ src/services/checkout.js
@@ -11,7 +11,7 @@ export function subscribe(id) {
 
 export function cancel(id) {
   return http
-    .post(`/payments/cancel/${id}`, {}, { headers: authHeader() })
+    .delete(`/payments/cancel/${id}`, {}, { headers: authHeader() })
     .then((response) => {
       return response.data
     })
@@ -51,7 +51,7 @@ export function setDefaultCard(data) {
 
 export function removeCard(data) {
   return http
-    .post('/payments/cards/remove', data, { headers: authHeader() })
+    .delete('/payments/cards/remove', data, { headers: authHeader() })
     .then((response) => {
       return response.data
     })

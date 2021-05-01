46430b4f6e795a99c1ca581054f879aa623771bf
Pablo <pabrengar@alum.us.es>
2021-05-01 10:28:40 +0200

    Update http-common

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,8 @@
+  
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: 'https://ebar-srv-sprint2.herokuapp.com/api/',
   headers: {
     'Content-type': 'application/json',
   },
